3037bdbca1d66eeae14ba5794ea3bac0089b2176
dcrearer <dcrearer@gmail.com>
2024-10-26 15:43:21 -0400

    initial commit

--- emr_datapipeline/emr_datapipeline_stack.py
+++ emr_datapipeline/emr_datapipeline_stack.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python3
+import os
+
+import aws_cdk as cdk
+
 from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
+  Stack,
+  aws_ec2,
+  aws_emr
 )
 from constructs import Construct
 
@@ -10,10 +15,77 @@ class EmrDatapipelineStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        EMR_CLUSTER_NAME = "cdk-demo"
+        
+        # Create a new VPC for EMR cluster
+        vpc = aws_ec2.Vpc(self, "EMRStackVPC",
+            max_azs=2,
+            gateway_endpoints={
+                "S3": aws_ec2.GatewayVpcEndpointOptions(
+                service=aws_ec2.GatewayVpcEndpointAwsService.S3
+                )
+            }
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "EmrDatapipelineQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Use the optional Parameters section to customize your templates. 
+        # Parameters enable you to input custom values to your template each time you create or update a stack.
+        emr_instances = aws_emr.CfnCluster.JobFlowInstancesConfigProperty(
+            core_instance_group=aws_emr.CfnCluster.InstanceGroupConfigProperty(
+                instance_count=2,
+                instance_type="m5.xlarge",
+                market="ON_DEMAND"
+            ),
+            ec2_subnet_id=vpc.public_subnets[0].subnet_id,
+            keep_job_flow_alive_when_no_steps=True, # After last step completes: Cluster waits
+            master_instance_group=aws_emr.CfnCluster.InstanceGroupConfigProperty(
+                instance_count=1,
+                instance_type="m5.xlarge",
+                market="ON_DEMAND"
+            ),
+            termination_protected=False
+        )
+
+        emr_version = self.node.try_get_context("emr_version") or "emr-7.2.0"
+
+        # Specifies and EMR cluster resource
+        emr_cfn_cluster = aws_emr.CfnCluster(self, "EMRCluster",
+            instances=emr_instances,
+            # In order to use the default role for `job_flow_role`, you must have already created it using the CLI or console
+            job_flow_role="EMR_EC2_DefaultRole",
+            name=EMR_CLUSTER_NAME,
+            service_role="EMR_DefaultRole",
+            applications=[
+                aws_emr.CfnCluster.ApplicationProperty(name="Hadoop"),
+                aws_emr.CfnCluster.ApplicationProperty(name="Hive"),
+                aws_emr.CfnCluster.ApplicationProperty(name="JupyterHub"),
+                aws_emr.CfnCluster.ApplicationProperty(name="Livy"),
+                aws_emr.CfnCluster.ApplicationProperty(name="Spark"),
+                aws_emr.CfnCluster.ApplicationProperty(name="JupyterEnterpriseGateway")
+            ],
+            bootstrap_actions=None,
+            configurations=[
+                aws_emr.CfnCluster.ConfigurationProperty(
+                classification="delta-defaults",
+                configuration_properties={
+                    "delta.enabled": "true"
+                }),
+                aws_emr.CfnCluster.ConfigurationProperty(
+                classification="hive-site",
+                configuration_properties={
+                    "hive.metastore.client.factory.class": "com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory"
+                }),
+                aws_emr.CfnCluster.ConfigurationProperty(
+                classification="spark-hive-site",
+                configuration_properties={
+                    "hive.metastore.client.factory.class": "com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory"
+                })
+            ],
+            ebs_root_volume_size=32,
+            log_uri="s3n://aws-logs-{account}-{region}/elasticmapreduce/".format(account=cdk.Aws.ACCOUNT_ID, region=cdk.Aws.REGION),
+            release_label=emr_version,
+            scale_down_behavior="TERMINATE_AT_TASK_COMPLETION",
+            visible_to_all_users=True
+        )
+
+        cdk.CfnOutput(self, 'EmrCluserName', value=emr_cfn_cluster.name)
+        cdk.CfnOutput(self, 'EmrVersion', value=emr_cfn_cluster.release_label)       
